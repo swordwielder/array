80ca99d5af72bd2df726d2f799cc016461eabdfa
swordwielder <qchen125@gmail.com>
2018-08-18 22:01:47 -0400

    ireir

--- factorial.java
+++ factorial.java
@@ -1,16 +1,23 @@
 import java.util.*;
 
-public class factorial
-{
+public class factorial{
 
-	public static void main (String [] args){
-
-		System.out.println("The factorial is: ");
-
-
+	public static void main (String [] args)
 	{
 
-	public static int fact(int n){
-		return n*fact(n-1);
+		System.out.println("enter a number ") ;
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+
+		System.out.println(" The factorial is " + factor(a));
+		
+	}
+
+	public static int factor(int n ){
+		if (n >1){
+			return  n *factor(n-1);
+		}else{
+			return 1;
+		}
 	}
 }
