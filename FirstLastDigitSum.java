316be86a92212bbf67dfac4039aba4015921b0ca
swordwielder <qchen125@gmail.com>
2019-01-15 13:22:25 -0500

    Adding another Udemy Exercise

---
+++ FirstLastDigitSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class FirstLastDigitSum {
+    
+    public static int sumFirstAndLastDigit(int number){
+        
+        if (number == 0){
+            return 0;
+        )else if (number < 0){
+            return -1;
+        }else{
+            String test = Integer.toString(number);
+            int a = Character.getNumericValue(test.charAt(0));
+            int b = Character.getNumericValue(test.charAt(test.length()-1));
+            return a+b;       
+        }
+    }  
+}
