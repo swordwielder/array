d37246aacaafdaf315c4bc37e1036b7f9aea8c5e
swordwielder <qchen125@gmail.com>
2019-01-15 18:34:28 -0500

    5th Udemy java exercise

---
+++ LastDigitChecker.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class LastDigitChecker {
+    // write your code here
+    
+    public static boolean hasSameLastDigit(int a, int b, int c){
+        if (a >1000 || a <10 || b >1000 || b <10 || c>1000 || c<10){
+            return false;
+        }else{
+            
+            return true;
+        }
+        
+        
+    }
+}
